d70ea1aa5f35834c7d0774a75fb46b6d8f075bf1
Dylan Osborn <dylanosborn123@hotmail.com>
2024-02-05 18:49:56 -0600

    Updated models and index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const routes = require('./routes');
+
+// Connect to MongoDB
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-db';
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
